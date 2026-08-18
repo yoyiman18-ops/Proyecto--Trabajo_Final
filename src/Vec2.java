52d9b72ef5a4c231801a67473b455eb279960c5a
valentindurante <valentindurante@gmail.com>
2026-08-18 13:21:11 -0300

    Añade javadoc a Vec2

--- src/Vec2.java
+++ src/Vec2.java
@@ -1,3 +1,6 @@
+/**
+ * Almacena dos componentes (x,y) de tipo primitive double.
+ */
 public class Vec2 implements Cloneable {
 
     public double x,y;
@@ -17,36 +20,50 @@ public class Vec2 implements Cloneable {
     // condiciones y son públicos, solo complica el acceso.
     // si se quiere poner el vector privado que se haga en la clase que
     // lo instancie
-
     public double getX() {
         return x;
     }
     public double getY() {
         return y;
     } 
-
     public void setX(double x) {
         this.x = x; 
     }
-
     public void setY(double y) {
         this.y = y;
     }
     */
 
+    /*
+     * Convierte el vector en un vector unitario.
+     * Mantiene la dirección y sentido originales, pero la magnitud pasa a ser 1.
+     * No hace nada si el vector ya era unitario, o si equivale a (0,0).
+     */
+    public void normalizar() {
     // esto es para la direccion de las entidades, que se puede representar
     // como un vector normalizado de 2 componentes
-    public void normalizar() {
 
         // en caso de ser (0,0) no hace nada
         if (x != 0 || y != 0) {
-        // magnitud = raiz cuadrada(x**2 + y**2)
-        double magnitud = Math.sqrt(x * x + y * y);
-        // vector normalizado: todas las componentes divididas por la magnitud del vec
-        x = x/magnitud;
-        y = y/magnitud;
-        // System.out.printf("vec normalizado: %f -- %f%n",this.x,this.y);
+            // magnitud = raiz cuadrada(x**2 + y**2)
+            double magnitud = Math.sqrt(x * x + y * y);
+            // vector normalizado: todas las componentes divididas por la magnitud del vec
+            if (magnitud != 1) {
+                x = x/magnitud;
+                y = y/magnitud;
+            }
         }
+    }
+
+    public Vec2 normalizado() {
+        if (x != 0 || y != 0) {
+            double magnitud = Math.sqrt(x * x + y * y);
+            if (magnitud != 1) { 
+                x = x/magnitud;
+                y = y/magnitud;
+            }
+        }
+        return this;
     }
 
     @Override
