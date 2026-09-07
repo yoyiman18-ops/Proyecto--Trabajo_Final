d7959583cddeb0c28012383898eb767e8dca8b78
valentindurante <valentindurante@gmail.com>
2026-09-07 17:12:40 -0300

    Implementacion perliminar de colisiones, usable

--- src/motor/colisiones/ParEntidades.java
+++ src/motor/colisiones/ParEntidades.java
@@ -4,7 +4,7 @@ import modelo.Entidad;
 public record ParEntidades(Entidad primera, Entidad segunda) {
 
     public ParEntidades(Entidad primera, Entidad segunda) {
-        if (primera == null || segunda == null) { throw new IllegalArgumentException("No puede haber entidad nula en par de colision");}
+        if (primera == null || segunda == null) { throw new IllegalArgumentException("No puede haber entidad nula en par");}
         if (primera.compareTo(segunda) >= 0) {
         this.primera = primera;
         this.segunda = segunda;
